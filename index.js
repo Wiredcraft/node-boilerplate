ad045d9deb8d98af67be9d45c1b26c97db860d24
Makara Wang <makara15@gmail.com>
2012-12-05 13:00:05 +0800

    Progress on Carcass: the http server.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-require('blah').start();
+// .
+var Server = require('carcass').load('servers', 'http');
+var server = new Server();
+server.start(function() {
+    console.log('Express server listening on port 3000.');
+});
