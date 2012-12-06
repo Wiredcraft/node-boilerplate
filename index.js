f6382e5a25b289ed8e307d623618bdf66bfa2f1e
Makara Wang <makara15@gmail.com>
2012-12-06 15:41:37 +0800

    Carcass: add debug and comments.

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-var carcass = require('carcass');
-
-// .
-var server = new carcass.servers.Http();
-
-// .
-server.start(function() {
-    console.log('Express server listening on port 3000.');
-});
+// Nothing for now.
